0a524cd508cd4618180542e2d989d0785b882f7a
SentsovSA <sentsov13@gmail.com>
2023-12-08 19:37:51 +0300

    ready

--- src/main/java/com/example/timajfx/Controller.java
+++ src/main/java/com/example/timajfx/Controller.java
@@ -1,14 +1,27 @@
 package com.example.timajfx;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableArray;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 
-public class HelloController {
+public class Controller {
     @FXML
-    private Label welcomeText;
+    private ComboBox<String> comboBox = new ComboBox<>();
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private Button button;
+
+    @FXML
+    private TextField textField;
+
+    @FXML
+    void onClick(ActionEvent event) {
+            comboBox.getItems().add(textField.getText());
     }
 }
--- src/main/java/com/example/timajfx/Main.java
+++ src/main/java/com/example/timajfx/Main.java
@@ -1,18 +1,19 @@
 package com.example.timajfx;
 
 import javafx.application.Application;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 465, 203);
+        stage.setTitle("Lab3!");
         stage.setScene(scene);
         stage.show();
     }
